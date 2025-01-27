e209ea0d4c2f0dc6a1c92570b3e2780ca5f39ae4
ryosuke-horie <r_horie@sincereed.com>
2025-01-27 23:55:09 +0900

    feat: GitHub Actions用にIssue番号を取得して表示する機能を追加

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
+// GitHub ActionsのIssue作成時に実行されることを想定
+// Issue番号を取得し表示する
 func main() {
-	fmt.Println("Hello, World!")
+	eventPath, exists := os.LookupEnv("GITHUB_EVENT_PATH")
+	if !exists {
+		fmt.Println("環境変数 GITHUB_EVENT_PATH が見つかりません")
+		return
+	}
+	fmt.Println("GITHUB_EVENT_PATH:", eventPath)
+
+	// eventPathからファイルを読み込む
+	file, err := os.Open(eventPath)
+	if err != nil {
+		fmt.Println("ファイルが開けませんでした")
+		return
+	}
+	defer file.Close()
+
+	// Issue番号を取得
+	var issueNumber string
+	fmt.Fscanf(file, `{"issue":{"number":%s`, &issueNumber)
+	fmt.Println("Issue番号:", issueNumber)
 }
