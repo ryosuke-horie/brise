415ed8e2d30e25bf4ed79c75bcbdac3c21676aa1
ryosuke-horie <r_horie@sincereed.com>
2025-01-28 00:07:52 +0900

    feat: GitHub Actions実行環境でのIssue番号取得機能を追加

--- main.go
+++ main.go
@@ -11,17 +11,23 @@ import (
 // GitHub ActionsのIssue作成時に実行されることを想定
 // Issue番号を取得し表示する
 func main() {
+	// GitHub Actions実行環境でのイベント情報が格納されているファイルパスを取得
 	eventPath := os.Getenv("GITHUB_EVENT_PATH")
 	if eventPath == "" {
 		log.Fatal("GITHUB_EVENT_PATH is not set")
 	}
 
+	// Issue番号を取得
+	issueNumber := getIssueNumber(eventPath)
+	fmt.Println("Issue Number:", issueNumber)
+}
+
+func getIssueNumber(eventPath string) string {
 	cmd := exec.Command("sh", "-c", fmt.Sprintf("cat %s | jq .issue.number", eventPath))
 	output, err := cmd.Output()
 	if err != nil {
 		log.Fatalf("コマンド実行に失敗しました。: %v", err)
 	}
 
-	issueNumber := strings.TrimSpace(string(output))
-	fmt.Printf("Issue number: %s\n", issueNumber)
+	return strings.TrimSpace(string(output))
 }
